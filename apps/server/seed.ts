ca5d100565cc2431059e6458a6726f1d398c8d80
deaMorta <freelance.starterpack08@gmail.com>
2023-05-06 05:24:41 +0800

    Update seed.ts

--- apps/server/seed.ts
+++ apps/server/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import argon2 from 'argon2'
-import {UserType} from 'commons'
+import {UserType} from '@app/commons'
 
 const prisma = new PrismaClient();
 
